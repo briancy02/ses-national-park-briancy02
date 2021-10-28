fa6a71912aa88f888733fe70e5de50fd7d92b9a9
briancy02 <>
2021-10-28 14:31:58 +0000

    added header

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header'
 
 function App() {
   return (
-    <main>
-      React⚛️ + Vite⚡ + Replit🌀
-    </main>
+    <div>
+      <Header />
+    </div>
   );
 }
 
