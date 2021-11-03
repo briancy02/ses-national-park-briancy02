2ce9aff12cdd767e4e953bdc8dc6f07e67e067d3
briancy02 <>
2021-11-03 13:36:03 +0000

    added Header and Feed

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header'
-import Feed from './components/Feed'
 
 function App() {
   return (
     <div>
       <Header />
-      <Feed />
     </div>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className="header">
+      <h1>Parkxplorers</h1>
+      <p> Explore national parks based on your favorite activities! </p>
+    </div>
+  )
+}
+
+export default Header
