190e8cb976baa8baca6926dcaf8b59b9c1568d05
briancy02 <>
2021-11-06 14:09:34 +0000

    added default card component to feed

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header'
+import Feed from './components/Feed'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
     <div>
       <Header />
+      <Feed />
     </div>
   );
 }
