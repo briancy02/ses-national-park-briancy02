ac8751da16d81b980edeb5a8b64dca9dda06e3f9
briancy02 <>
2021-11-07 00:42:10 +0000

    figured out how to make api requests

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -2,8 +2,35 @@ import React from 'react'
 import './Feed.css'
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
+import {useState, useEffect} from 'react'
 
 const Feed = () => {
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [items, setItems] = useState([]);
+
+  // Note: the empty deps array [] means
+  // this useEffect will run once
+  // similar to componentDidMount()
+  useEffect(() => {
+    fetch('https://developer.nps.gov/api/v1/activities?api_key=mmoyV9anVebj4jDnqXwPIO5sCkwRfEORKTDNagTp', { method: 'GET' })
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setItems(result);
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      )
+  }, [])
+   
+
   return (
     <div className="feed">
       <Card style={{ width: '18rem' }}>
@@ -12,7 +39,9 @@ const Feed = () => {
           <Card.Title>Card Title</Card.Title>
           <Card.Text>
             Some quick example text to build on the card title and make up the bulk of
-            the card's content.
+            the card's content
+            {error}{isLoaded}{items.total}
+
           </Card.Text>
           <Button variant="primary">Go somewhere</Button>
         </Card.Body>
